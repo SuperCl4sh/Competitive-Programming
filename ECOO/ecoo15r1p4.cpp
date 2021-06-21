ef7270932767dce473dc3135af9b468d3b4c73db
Seyon Sivatharan <superclash554@gmail.com>
2021-06-20 23:19:22 -0400

    Added ECOO solutions

---
+++ ECOO/ecoo15r1p4.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define PF pop_front
+#define PB pop_back
+#define P push
+#define F first
+#define S second
+#define boost ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define PI 3.14159265
+#define len(s) (int)s.length()
+#define sz(s) (int)s.size()
+#define sp(x) fixed << setprecision(x)
+#define all(a) a.begin(),a.end()
+#define mem(ar,a) memset(ar,a,sizeof(ar))
+#define MT make_tuple
+#define FOR(i,a,n) for (int i=a;i<n;i++)
+#define vt vector
+typedef long long ll;
+typedef string str;
+typedef pair<int,int> pii;
+typedef pair<ll,ll>pll;
+typedef pair<long double, long double>pld;
+typedef pair<double,double>pdd;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<ld,ld>pld;
+typedef pair<str,str>pss;
+typedef pair<char,char>pcc;
+typedef pair<long,long>pl;
+typedef pair<bool,bool>pbb;
+const ll MOD = 1000000007LL;
+const ll MOD2=998244353LL;
+const ll high= (ll)(1e15);
+void read(auto &num) {register int Z;num=0;bool negat=0;Z=getchar();if (Z=='-') {negat=1;Z=getchar();}for(;(Z>47&&Z<58);Z=getchar()){num=num*10+Z-48;}if(negat){num*=-1;}}
+void Read(auto &num) {register int Z;num=0;bool negat=0;Z=getchar_unlocked();if (Z=='-') {negat=1;Z=getchar_unlocked();}for(;(Z>47&&Z<58);Z=getchar_unlocked()){num=num*10+Z-48;}if(negat){num*=-1;}}
+ll modmul(ll a, ll b, ll m) {ll ans=a*b-m*ll(1.L/m*a*b);return ans+m*(ans<0)-m*(ans>=m);}
+ll Pow(ll a, ll b, ll mod) {ll ans=1;for (; b;a=modmul(a,a,mod),b/=2) {if (b&1) ans=modmul(ans,a,mod);}return ans;}
+ll Pow(ll a, ll b) {ll ret=1;while (b) {if (b&1) ret*=a;a*=a;b>>=1;}return ret;}
+
+/*--------------------------------------------------------------PROGRAM START-------------------------------------------------------------------------*/
+
+vt<str>possib={"ook","ookook","oog","ooga","ug","mook","mookmook","oogam","oogum","ugug"};
+str s;
+ll dp[355];
+int solve(int ind) {
+	if (ind==sz(s)) return 1;
+	else if (ind>sz(s)) return 0;
+	else if (dp[ind]!=-1) return dp[ind];
+	int ret=0;
+	FOR (i,0,10) {
+		if (ind+sz(possib[i])<=sz(s)) {
+			if (s.substr(ind,sz(possib[i]))==possib[i]) ret+=solve(ind+sz(possib[i]));
+		}
+	}
+	return dp[ind]=ret;
+}
+
+int main() {
+	boost;
+	
+	FOR (i,0,10) {
+		mem(dp,-1);
+		cin >> s;
+		cout << solve(0) << "\n";
+	
+	}
+	
+	
+	
+	return 0;
+}
---
+++ ECOO/ecoo17r3p1.cpp
@@ -0,0 +1,37 @@
+#pragma GCC optimize "Ofast"
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef string str;
+typedef pair<int,int> pii;
+typedef pair<ll,ll>pll;
+
+
+ int main() {
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int a,f,d,tot=0,soldDay;
+    for (int i=0;i<10;i++) {
+        cin >> f >> d;
+        tot=0;
+        vector<int>ar(f);
+        for (int w=0;w<d;w++) {
+            soldDay=0;
+            for (int g=0;g<f;g++) {
+                cin >> a;
+                ar[g]+=a;
+                soldDay+=a;
+            }
+            if (soldDay%13==0) {
+                tot+=soldDay/13;
+            }
+        }
+        for (int e=0;e<f;e++) {
+            if (ar[e]%13==0) {
+                tot+=ar[e]/13;
+            }
+        }
+        cout << tot << endl;
+    }
+
+    return 0;
+ }
